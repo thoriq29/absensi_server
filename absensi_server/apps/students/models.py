2d6fde206ae1d8286a3615f1058ac7a7e9494e68
thoriq29 <oiq2908@gmail.com>
2019-02-16 15:51:37 +0700

    create model student

--- absensi_server/apps/students/models.py
+++ absensi_server/apps/students/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+from model_utils import Choices
+
+
+class Students(models.Model):
+    name = models.CharField(max_length=50)
+    NIS = models.CharField(max_length=50)
+    GRADE = Choices(
+        (1, 'sepuluh', 'X'),
+        (2, 'sebelas', 'XI'),
+        (3, 'duabelas', 'XII')
+    )
+    ACADEMIC_FIELD = Choices(
+        (1, 'RPL', 'Rekayasa Perangkat Lunak'),
+        (2, 'TKJ', 'Teknik Komputer Jaringan'),
+        (3, 'AKT', 'Akuntansi'),
+        (4, 'AP', 'Administrasi Perkantoran'),
+        (5, 'TN', 'Tata Niaga'),
+    )
+    academic_field = models.PositiveSmallIntegerField(choices=ACADEMIC_FIELD)
+    grade = models.PositiveSmallIntegerField(choices=GRADE)
+    GENDER = Choices(
+        (1, 'male', 'Male'),
+        (2, 'female', 'eFemale')
+    )
+    gender = models.PositiveSmallIntegerField(choices=GENDER)
+    email = models.EmailField('email address', unique=True, max_length=254)
